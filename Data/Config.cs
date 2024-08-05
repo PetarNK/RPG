a93e6b5edf71dff87d5f6ecb93a9de535e98e25c
PetarNK <89004582+PetarNK@users.noreply.github.com>
2024-08-05 19:44:11 +0300

    Create config file to connect to the Database and create race selection and points allocation system

---
+++ Data/Config.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG.Data
+{
+    public class Config
+    {
+        public const string ConnectionString = @"Server=localhost;Database=RPG;Integrated Security=true;";
+    }
+}
--- StartUp.cs
+++ StartUp.cs
@@ -1,13 +1,24 @@
 ﻿
+using RPG.Menu;
+using RPG.Models.CharacterInfo;
+using RPG.Models.Monster;
 using RPG.Utilities;
 
 namespace RPG
 {
-    internal class StartUp
+    public class StartUp
     {
+        static Screen currentScreen = Screen.MainMenu;
+        static Character player;
+        static List<Monster> monsters = new List<Monster>();
+        static Random random = new Random();
         static void Main(string[] args)
         {
+
             Greeter.ShowMainMenu();
+
+
+            CharacterSelect.ShowCharacterSelect(currentScreen, player);
 
             Drawer.DrawGameField();
         }
